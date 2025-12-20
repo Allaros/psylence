c3327170119ac7c8a4461dfa21305378beba2cc9
Sergey Dubskiy <dubskiy_03@mail.ru>
2025-12-20 14:06:28 +0300

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,70 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import { FlatCompat } from '@eslint/eslintrc';
+import path from 'node:path';
+import url from 'node:url';
+import js from '@eslint/js';
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+   ...nextVitals,
+   ...nextTs,
+   ...new FlatCompat({
+      baseDirectory: path.dirname(url.fileURLToPath(import.meta.url)),
+      recommendedConfig: js.configs.recommended,
+      allConfig: js.configs.all,
+   }).config({
+      extends: ['eslint-config-standard', 'prettier'],
+   }),
+   {
+      rules: {
+         'import/order': [
+            'error',
+            {
+               groups: [
+                  'builtin',
+                  'external',
+                  'internal',
+                  ['parent', 'sibling'],
+                  'index',
+                  'object',
+               ],
+
+               'newlines-between': 'always',
+
+               pathGroups: [
+                  {
+                     pattern: '@app/**',
+                     group: 'external',
+                     position: 'after',
+                  },
+               ],
+
+               pathGroupsExcludedImportTypes: ['builtin'],
+
+               alphabetize: {
+                  order: 'asc',
+                  caseInsensitive: true,
+               },
+            },
+         ],
+         'comma-dangle': 'off',
+      },
+   },
+   {
+      files: ['**/*.ts', '**/*.tsx'],
+      rules: {
+         'no-undef': 'off',
+      },
+   },
+   // Override default ignores of eslint-config-next.
+   globalIgnores([
+      // Default ignores of eslint-config-next:
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+   ]),
 ]);
 
 export default eslintConfig;
